6213912ccba10c269bbff74492d3cdbfdd22a510
Mahmoud amr <113695259+mahmod5000@users.noreply.github.com>
2023-10-31 15:28:34 +0200

    after end of applications of satck

--- main.cpp
+++ main.cpp
@@ -3,17 +3,102 @@
 #include<string>
 using namespace std;
 #define Endl endl;
-// infix to postfix function
-string infixToPostfix(string expression)
+// infix to postfix function // important
+
+
+
+bool IsOperand(char c)
 {
-    string postFix="";
-    for (int i = 0; i < expression.size(); ++i)
+    if(c>='a'&& c<='z'){return true ; }
+    else if(c>='0'&&c<='9'){return  true ;}
+    else if(c>='A' && c <='Z'){return true;}
+    else {return false ;}
+}
+bool IsOperator(char c)
+{
+    switch (c)
     {
+        case '+':
+            return true;break;
+        case '-':
+            return true;break;
+        case '*':
+            return true;break;
+        case '/':
+            return true;break;
+        case '^':
+            return true;break;
+        case '$':
+            return true;break;
+        default :
+            return false;break;
+    }
+}
+bool IsRightAcc(char op)
+{
+    if(op == '$'){return true;}
+    return false;
+}
+int GetOperatorWight(char op )
+{
+    int wight = -1 ;
+    switch(op)
+    {
+        case '+':
+        case '-':
+            wight = 1;break;
+        case'*':
+        case'/':
+            wight=2;
+        case '$':
+            wight=3;
+    }
+    return wight;
+}
+int HasHeightPrecedence(char op1 ,char op2)
+{
+    int wight1= GetOperatorWight(op1);
+    int wight2= GetOperatorWight(op2);
+    if(wight1 == wight2)
+    {
+        if(IsRightAcc(op1)){return false;}
+        return true;
+    }
+    return wight1 > wight2;
+}
+string infixToPostfix(string expression) {
+    Stack<char> s;
+    string postFix = "";
+    for (int i = 0; i < expression.size(); ++i) {
+        if (expression[i] == ' ' || expression[i] == ',') { continue; }
+        else if (IsOperator(expression[i])) {
+            while (!s.isempty() && s.getTop() != '(' && HasHeightPrecedence(s.getTop(), expression[i])) {
+                postFix += s.getTop();
+                s.pop();
+            }
+            s.push(expression[i]);
+        } else if (IsOperand(expression[i])) {
+            postFix += expression[i];
+        } else if (expression[i] == '(') {
+            s.push(expression[i]);
+        } else if (expression[i] == ')') {
+            while (!s.isempty() && s.getTop() != '(') {
+                postFix += s.getTop();
+                s.pop();
+            }
+            s.pop();
+        }
 
     }
+    while (!s.isempty()) {
+        postFix += s.getTop();
+        s.pop();
+    }
+    s.pop();
+    return postFix;
 }
 int main()
 {
-
+cout<<infixToPostfix("5+5");
 
 }
