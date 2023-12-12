dc8674066bc03eb1a9103270baede5e2ad8218bd
Mahmoud amr <113695259+mahmod5000@users.noreply.github.com>
2023-12-12 20:49:49 +0200

    new adds

--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
 #include<iostream>
-#include"list.h"
+#include "list.h"
 
 using namespace std;
 #define Endl endl;
 
 int main()
 {
-   linked_list<int>t;
-   t.push_front(5);
-   t.push_back(10);
-   t.insertAtPosition(2,15);
-   t.print();
-
-
 
 }
