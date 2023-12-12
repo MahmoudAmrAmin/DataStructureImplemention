a51e0533d51cda7a0b138652e949a755faad4515
Mahmoud amr <113695259+MahmoudAmrAmin@users.noreply.github.com>
2023-12-12 20:48:50 +0200

    Update list.h add removes functions

--- list.h
+++ list.h
@@ -1,9 +1,3 @@
-//
-// Created by mahmod on 11/13/2023.
-//
-
-#ifndef DATASTRUCTURES_IMPLEMENT_LIST_H
-#define DATASTRUCTURES_IMPLEMENT_LIST_H
 #include<iostream>
 using namespace std ;
 template<class t>class linked_list
@@ -19,13 +13,23 @@ private:
     int count;
 public:
     linked_list();
+    t front();
+    t back();
     bool empty();
     void push_front(t element);
     void push_back(t element);
     void insertAtPosition(t pos , t element);
+    void pop_front();
+    void pop_back();
     void print();
+    int size ();
+    void removeByValue(t element);
 };
 
+template<class t> int linked_list<t>::size()
+{
+    return count;
+}
 template<class t> linked_list<t>::linked_list()
 {
     count = 0 ;
@@ -34,9 +38,8 @@ template<class t> linked_list<t>::linked_list()
 
 template<class t> bool linked_list<t>::empty()
 {
-    return count ==0;
+    return count <=0;
 }
-
 template<class t> void linked_list<t>::push_front(t element)
 {
     node * newNode = new node;
@@ -62,9 +65,9 @@ template<class t > void linked_list<t>::push_back(t element)
     }
     else
     {
-      last->next= newNode;
-      last = newNode;
-      last->next= NULL;
+        last->next= newNode;
+        last = newNode;
+        last->next= NULL;
     }
     count++;
 }
@@ -108,5 +111,92 @@ template<class t>void linked_list<t>::print()
     }
 }
 
+template<class t> void linked_list<t>::pop_front() {
+    if(empty())
+    {
+        cout<<"underflow , list is empty "<<endl;
+    }
+    else if(size() == 1)
+    {
+        head = last =NULL;
+        count--;
+    }
+    else
+    {
+        node * temp = new node ;
+        temp = head ;
+        head = head->next ;
+        delete temp ;
+        count -- ;
+    }
 
-#endif //DATASTRUCTURES_IMPLEMENT_LIST_H
+}
+template<class t> t linked_list<t> ::front()
+{
+    if(empty())
+    {
+        cout<<"underflow , list is empty"<<endl;
+        return -1 ;
+    }
+    return head ->item;
+}
+
+template<class t> void linked_list<t>::pop_back()
+{
+    if(empty())
+    {
+        cout<<"underflow , list is empty "<<endl;
+    }
+    else if(size() == 1)
+    {
+        head = last =NULL;
+        count--;
+    }
+    else
+    {
+        node * curr  = new node, * temp = new node;
+        temp = head->next;
+        curr = head ;
+        while(temp != last)
+        {
+            curr = temp;
+            temp= temp->next ;
+        }
+        last = curr ;last ->next = NULL ;
+        delete temp , curr;
+
+        count --;
+    }
+}
+
+template<class t> t linked_list<t> ::back()
+{
+    if(empty())
+    {
+        cout<<"underflow , list is empty"<<endl;
+        return -1 ;
+    }
+    return last ->item;
+}
+
+template<class t> void linked_list<t>::removeByValue(t element)
+{
+    if(empty())
+    {
+        cout<<"underflow"<<endl;return ;
+    }
+    node * curr = new node , * pre = new node;
+    if(head ->item == element)
+    {
+        curr = head ;
+        pre = head ->next ;
+        head = pre ;
+        delete curr ;
+        count -- ;
+        if( ! count ){last = NULL ; }
+    }
+    else
+    {
+
+    }
+}
